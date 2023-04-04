702d503edd1e63db6c1635df4d6b502ef7500f69
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-04 14:35:02 +0200

    add while cicle in js script

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+//lista della spesa con ciclo while
+
+let producList = [];
 
 
+
+while(true){
+
+    let itemList = prompt("inserisci un articolo alla lista oppure premi * per completare");
+    if(itemList.toLowerCase() === '*'){
+        break;
+    };
+
+    producList.push(itemList);
+};
+
+
+console.log('la tua lista della spesa:' + producList);
